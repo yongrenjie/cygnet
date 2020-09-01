750c3c906788d611903da12f4baccd02abf7ceb6
Jonathan Yong <yongrenjie@gmail.com>
2020-09-01 17:33:11 +0800

    Fix setuptools entry point

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ setuptools.setup(
     ],
     # Entry points (command-line)
     entry_points = {
-        'console_scripts': ['peep=peeplatex.main:main'],
+        'console_scripts': ['peep=peeplatex.startup:main'],
     }
 )
