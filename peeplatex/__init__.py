f3e4e4941e6e3e83aa3453172b1369cfa2dbab78
Jonathan Yong <yongrenjie@gmail.com>
2020-10-11 11:23:05 +0100

    Add cite() convenience function

---
+++ peeplatex/__init__.py
@@ -0,0 +1,8 @@
+from .peepcls import DOI
+
+
+def cite(doi, type="bib"):
+    """
+    This is a convenience function. Defaults to BibLaTeX citation style.
+    """
+    return DOI(doi).to_citation(type=type)
